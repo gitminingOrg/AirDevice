f9ff6fc133453b54441e581cacb0c0930922fd4a
Yiming Chen <630346810@qq.com>
2017-06-16 15:32:09 +0800

    update sql

--- reception/src/main/webapp/www/js/mall.js
+++ reception/src/main/webapp/www/js/mall.js
@@ -1,3 +1,12 @@
-app.controller('MallCtrl', function($scope) {
-
+app.controller('MallCtrl', function($scope, $http) {
+//	$http.get('/reception/goods/consumer/list').success(function(response){
+//		if(response.status == 1){
+//			$scope.goodsList = response.contents.goodsList
+//			
+//		}
+//	});
+	//$http.get('/reception/goods/{goodsID}/cover').success(function(response){});
+	$scope.goodsList = [1,2,3,4,5]
+	
+	$scope.goodsDetail = function(){alert('aaa')}
 })
