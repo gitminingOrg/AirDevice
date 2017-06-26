4ca8c63a084ed00795a7abba256920a96e5790ec
zhangxufan <sunshinezxf@hotmail.com>
2017-06-26 13:38:40 +0800

    implementation of customer insert

--- reception/src/main/java/vip/service/impl/ConsumerServiceImpl.java
+++ reception/src/main/java/vip/service/impl/ConsumerServiceImpl.java
@@ -4,10 +4,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.plaf.BorderUIResource.CompoundBorderUIResource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import dao.ConsumerDao;
 import model.vip.Consumer;
@@ -15,38 +18,38 @@ import vip.service.ConsumerSerivce;
 import vo.vip.ConsumerVo;
 
 @Service
-public class ConsumerServiceImpl implements ConsumerSerivce{
+public class ConsumerServiceImpl implements ConsumerSerivce {
 	private Logger logger = LoggerFactory.getLogger(ConsumerServiceImpl.class);
-	
+
 	@Autowired
 	private ConsumerDao consumerDao;
-	
+
 	@Override
 	public ConsumerVo login(String phone, String password) {
 		Map<String, Object> condition = new HashMap<>();
 		condition.put("customerPhone", phone);
 		List<ConsumerVo> list = consumerDao.query(condition);
-		if(list.isEmpty()) {
+		if (list.isEmpty()) {
 			return null;
 		}
-		return list.get(0); 
+		return list.get(0);
 	}
-	
+
 	@Override
 	public ConsumerVo login(String wechat) {
 		Map<String, Object> condition = new HashMap<>();
 		condition.put("wechat", wechat);
 		List<ConsumerVo> list = consumerDao.query(condition);
-		if(list.isEmpty()) {
+		if (list.isEmpty()) {
 			return null;
 		}
 		return list.get(0);
 	}
-	
+
 	@Override
 	public List<ConsumerVo> fetch(Map<String, Object> condition) {
 		List<ConsumerVo> list = consumerDao.query(condition);
-		if(list.isEmpty()) {
+		if (list.isEmpty()) {
 			return null;
 		}
 		return list;
@@ -54,7 +57,14 @@ public class ConsumerServiceImpl implements ConsumerSerivce{
 
 	@Override
 	public ConsumerVo create(Consumer consumer) {
-		
+		boolean status = consumerDao.insert(consumer);
+		if (status) {
+			Map<String, Object> condition = new HashMap<>();
+			if (!StringUtils.isEmpty(consumer.getWechat())) {
+				condition.put("wechat", consumer.getWechat());
+			}
+			return fetch(condition).get(0);
+		}
 		return null;
 	}
 }
