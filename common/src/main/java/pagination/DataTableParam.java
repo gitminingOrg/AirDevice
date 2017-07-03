2d0fb3262040e2bbf9062fb3a815f2f6b6a1922d
zhangxufan <sunshinezxf@hotmail.com>
2017-07-03 23:41:14 +0800

    add necesary component for management side

---
+++ common/src/main/java/pagination/DataTableParam.java
@@ -0,0 +1,92 @@
+package pagination;
+
+/**
+ * Created by sunshine on 4/20/16.
+ */
+public class DataTableParam {
+    /*display start point in the current data set*/
+    private int iDisplayStart;
+
+    /*number of records that the table can display in the current draw*/
+    private int iDisplayLength;
+
+    /*global search field*/
+    private String sSearch;
+
+    /*information for DataTables to use for rendering*/
+    private String sEcho;
+
+    private String params;
+
+    private int iSortCol_0;
+
+    private String sSortDir_0;
+
+
+    public int getiDisplayStart() {
+        return iDisplayStart;
+    }
+
+    public void setiDisplayStart(int iDisplayStart) {
+        this.iDisplayStart = iDisplayStart;
+    }
+
+    public int getiDisplayLength() {
+        return iDisplayLength;
+    }
+
+    public void setiDisplayLength(int iDisplayLength) {
+        this.iDisplayLength = iDisplayLength;
+    }
+
+    public String getsSearch() {
+        return sSearch;
+    }
+
+    public void setsSearch(String sSearch) {
+        this.sSearch = sSearch;
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public void setParams(String params) {
+        this.params = params;
+    }
+
+    public String getsEcho() {
+        return sEcho;
+    }
+
+    public void setsEcho(String sEcho) {
+        this.sEcho = sEcho;
+    }
+
+    public int getiSortCol_0() {
+        return iSortCol_0;
+    }
+
+    public void setiSortCol_0(int iSortCol_0) {
+        this.iSortCol_0 = iSortCol_0;
+    }
+
+    public String getsSortDir_0() {
+        return sSortDir_0;
+    }
+
+    public void setsSortDir_0(String sSortDir_0) {
+        this.sSortDir_0 = sSortDir_0;
+    }
+
+    public int getPageNum() {
+        int num = 0;
+        if (getiDisplayStart() != 0) {
+            num = getiDisplayStart() / getiDisplayLength();
+            return num;
+        }
+        return num;
+    }
+
+
+}
