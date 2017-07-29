1fe9962e2efe701b666cf9c8427a121e3c8eaae3
zhangxufan <sunshinezxf@hotmail.com>
2017-07-30 03:19:18 +0800

    add order vo

---
+++ common/src/main/java/vo/order/OrderVo.java
@@ -0,0 +1,123 @@
+package vo.order;
+
+public class OrderVo {
+	private String orderId;
+	
+	private String orderNo;
+	
+	private String buyerName;
+	
+	private String buyerAccount;
+	
+	private String receiverName;
+	
+	private String receiverPhone;
+	
+	private String receiverAddress;
+	
+	private String coupon;
+	
+	private String goodsTitle;
+	
+	private String goodsKind;
+	
+	private String payTime;
+	
+	private String channel;
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public String getBuyerName() {
+		return buyerName;
+	}
+
+	public void setBuyerName(String buyerName) {
+		this.buyerName = buyerName;
+	}
+
+	public String getBuyerAccount() {
+		return buyerAccount;
+	}
+
+	public void setBuyerAccount(String buyerAccount) {
+		this.buyerAccount = buyerAccount;
+	}
+
+	public String getReceiverName() {
+		return receiverName;
+	}
+
+	public void setReceiverName(String receiverName) {
+		this.receiverName = receiverName;
+	}
+
+	public String getReceiverPhone() {
+		return receiverPhone;
+	}
+
+	public void setReceiverPhone(String receiverPhone) {
+		this.receiverPhone = receiverPhone;
+	}
+
+	public String getReceiverAddress() {
+		return receiverAddress;
+	}
+
+	public void setReceiverAddress(String receiverAddress) {
+		this.receiverAddress = receiverAddress;
+	}
+
+	public String getCoupon() {
+		return coupon;
+	}
+
+	public void setCoupon(String coupon) {
+		this.coupon = coupon;
+	}
+
+	public String getGoodsTitle() {
+		return goodsTitle;
+	}
+
+	public void setGoodsTitle(String goodsTitle) {
+		this.goodsTitle = goodsTitle;
+	}
+
+	public String getGoodsKind() {
+		return goodsKind;
+	}
+
+	public void setGoodsKind(String goodsKind) {
+		this.goodsKind = goodsKind;
+	}
+
+	public String getPayTime() {
+		return payTime;
+	}
+
+	public void setPayTime(String payTime) {
+		this.payTime = payTime;
+	}
+
+	public String getChannel() {
+		return channel;
+	}
+
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+}
