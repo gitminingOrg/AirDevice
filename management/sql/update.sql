f03be298e44ae6223b6bf19a74e62ae15ba6b825
xxh13 <15850781776@163.com>
2017-10-25 23:43:37 +0800

    数据库的sql语句

---
+++ management/sql/update.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `pre_binding`;
+CREATE TABLE `pre_binding` (
+  `bind_id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` char(255) NOT NULL,
+  `code_id` varchar(255) NOT NULL,
+  `block_flag` int(11) NOT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`bind_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
