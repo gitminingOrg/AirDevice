97720c1bd6863f4ad694c739fd914a1a0bc63915
xxh13 <15850781776@163.com>
2017-10-28 17:09:30 +0800

    判断获取的数据是否为空

---
+++ management/src/main/java/controller/LocationController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import config.ManagementConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import utils.HttpDeal;
+import utils.ResponseCode;
+import utils.ResultData;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Created by hushe on 2017/10/28.
+ */
+@RestController
+@RequestMapping("/location")
+public class LocationController {
+    private Logger logger = LoggerFactory.getLogger(LocationController.class);
+
+    @RequestMapping(method =  RequestMethod.GET, value = "/ip")
+    public ResultData ip2city(String ip){
+        ResultData result = new ResultData();
+
+        String url = new StringBuffer
+                ("http://apis.map.qq.com/ws/location/v1/ip").append("?ip=").append(ip)
+                .append("&key=").append(ManagementConfig.getValue("tencent_map_key"))
+                .toString();
+
+        String response = HttpDeal.getResponse(url);
+        JSONObject json = JSON.parseObject(response);
+        try {
+            JSONObject jo = new JSONObject();
+            jo.put("nation", json.getJSONObject("result").getJSONObject("ad_info").get("nation"));
+            jo.put("province", json.getJSONObject("result").getJSONObject("ad_info").get("province"));
+            jo.put("city", json.getJSONObject("result").getJSONObject("ad_info").get("city"));
+            result.setData(jo);
+        }catch (Exception e){
+            logger.error(e.getMessage());
+            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+            result.setDescription(e.getMessage());
+        }
+        return result;
+    }
+}
