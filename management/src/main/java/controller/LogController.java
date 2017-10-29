d30e7f06bc50256fdd50a060fd36e31b0bf58a28
zhangxufan <sunshinezxf@hotmail.com>
2017-10-30 00:47:22 +0800

    external interface log4user to be implemented

---
+++ management/src/main/java/controller/LogController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import utils.ResultData;
+
+@RestController
+@RequestMapping("/log")
+public class LogController {
+	private Logger logger = LoggerFactory.getLogger(LogController.class);
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/list")
+	public ResultData log4user() {
+		ResultData result = new ResultData();
+		
+		return result;
+	}
+	
+}
