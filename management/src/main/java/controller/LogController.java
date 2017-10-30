822ed9ea275b735b96816df2c02d33b523e11152
hushenghao <295065492@qq.com>
2017-10-30 16:58:11 +0800

    userLog

--- management/src/main/java/controller/LogController.java
+++ management/src/main/java/controller/LogController.java
@@ -1,24 +1,41 @@
 package controller;
 
+import org.omg.CORBA.Object;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import java.lang.Class;
 
 import service.UserLogService;
+import utils.ResponseCode;
 import utils.ResultData;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/log")
 public class LogController {
 	private Logger logger = LoggerFactory.getLogger(LogController.class);
+
+	private UserLogService userLogService;
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/list")
 	public ResultData log4user() {
 		ResultData result = new ResultData();
-		ResultData response = UserLogService.
+
+		Map<String, Object> condition = new HashMap<>();
+		//Map.put()?
+
+		ResultData response = userLogService.fetch(condition);
+		result.setResponseCode(response.getResponseCode());
+		if (response.getResponseCode() == ResponseCode.RESPONSE_OK){
+			result.setData(response.getData());
+		}
+		else if (response.getResponseCode() == ResponseCode.RESPONSE_ERROR){
+			result.setDescription(response.getDescription());
+		}
 		return result;
 	}
 	
