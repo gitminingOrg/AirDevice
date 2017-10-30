9768d6a1fa79179bdcfdef08351e1fc12c7940f5
xxh13 <15850781776@163.com>
2017-10-31 00:35:40 +0800

    手机端 monitor界面

---
+++ monitor/src/main/java/finley/monitor/controller/MobileMonitorController.java
@@ -0,0 +1,40 @@
+package finley.monitor.controller;
+
+
+import finley.monitor.service.CityPM25Service;
+import finley.monitor.service.MachineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import utils.ResponseCode;
+import utils.ResultData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/mobileMonitor")
+public class MobileMonitorController {
+
+    @Autowired
+    private MachineService machineService;
+
+    @Autowired
+    private CityPM25Service cityPM25Service;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{machine}")
+    public ModelAndView mobileMonitor(@PathVariable("machine") String machine) {
+        ModelAndView view = new ModelAndView();
+        Map<String, Object> condition = new HashMap<>();
+        condition.put("machineId", machine);
+        ResultData response = machineService.fetch(condition);
+        if (response.getResponseCode() == ResponseCode.RESPONSE_OK) {
+            view.addObject("machineId", machine);
+        }
+        view.setViewName("/mobile/item");
+        return view;
+    }
+}
