0bee504cbb745c2587d7dc957c5ab84bd860cbb5
hshydtj9405 <295065492@qq.com>
2017-11-07 12:51:08 +0800

    修改ip2city接口

--- management/src/main/java/controller/LocationController.java
+++ management/src/main/java/controller/LocationController.java
@@ -1,10 +1,12 @@
 package controller;
 
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import config.ManagementConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,11 +34,16 @@ public class LocationController {
         String response = HttpDeal.getResponse(url);
         JSONObject json = JSON.parseObject(response);
         try {
-            JSONObject jo = new JSONObject();
-            jo.put("nation", json.getJSONObject("result").getJSONObject("ad_info").getJSONObject("nation"));
-            jo.put("province", json.getJSONObject("result").getJSONObject("ad_info").getJSONObject("province"));
-            jo.put("city", json.getJSONObject("result").getJSONObject("ad_info").getJSONObject("city"));
-            result.setData(jo);
+            if(!StringUtils.isEmpty(json)){
+                JSONObject jo = new JSONObject();
+                jo.put("nation", json.getJSONObject("result").getJSONObject("ad_info").getJSONObject("nation"));
+                jo.put("province", json.getJSONObject("result").getJSONObject("ad_info").getJSONObject("province"));
+                jo.put("city", json.getJSONObject("result").getJSONObject("ad_info").getJSONObject("city"));
+                result.setData(jo);
+            }else {
+                result.setResponseCode(ResponseCode.RESPONSE_NULL);
+                result.setDescription("未获取到城市信息！");
+            }
         }catch (Exception e){
             logger.error(e.getMessage());
             result.setResponseCode(ResponseCode.RESPONSE_ERROR);
