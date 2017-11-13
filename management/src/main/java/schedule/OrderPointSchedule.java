a62dc8fd5cd25f0a25b3fe6ad796bd0f76592038
zhangxufan <sunshinezxf@hotmail.com>
2017-11-13 21:02:19 +0800

    add new schedule

---
+++ management/src/main/java/schedule/OrderPointSchedule.java
@@ -0,0 +1,12 @@
+package schedule;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OrderPointSchedule {
+	private Logger logger = LoggerFactory.getLogger(OrderPointSchedule.class);
+
+	public void schedule() {
+
+	}
+}
