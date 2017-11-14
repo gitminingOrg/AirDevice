15b7a64c5189dcd9c7fb53dd51c6554c3541642f
hshydtj9405 <295065492@qq.com>
2017-11-14 20:19:38 +0800

    debug

--- management/src/main/java/controller/LogController.java
+++ management/src/main/java/controller/LogController.java
@@ -1,8 +1,6 @@
 package controller;
 
-import com.sun.javafx.util.Logging;
 import model.userlog.UserLog;
-import org.apache.commons.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
