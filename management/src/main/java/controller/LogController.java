0db9500f28da9db9f4002e231cd3d0b2d3edf3a5
hshydtj9405 <295065492@qq.com>
2017-11-16 20:18:16 +0800

    debug

--- management/src/main/java/controller/LogController.java
+++ management/src/main/java/controller/LogController.java
@@ -2,7 +2,6 @@ package controller;
 
 import model.systemlog.SystemLog;
 import model.userlog.UserLog;
-import org.apache.commons.collections.map.HashedMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
