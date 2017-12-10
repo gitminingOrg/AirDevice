c370ddf63280e8eb6b09099975bea573d684cc80
zhangxufan <sunshinezxf@hotmail.com>
2017-12-10 14:53:28 +0800

    implement of service and controller for machine installation insight

--- management/src/main/java/dao/impl/InsightDaoImpl.java
+++ management/src/main/java/dao/impl/InsightDaoImpl.java
@@ -6,10 +6,12 @@ import model.machine.Insight;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import utils.IDGenerator;
 import utils.ResponseCode;
 import utils.ResultData;
 import vo.machine.InsightVo;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,9 +27,14 @@ public class InsightDaoImpl extends BaseDao implements InsightDao {
     @Override
     public ResultData insert(Insight insight) {
         ResultData result = new ResultData();
+        insight.setInsightId(IDGenerator.generate("IIS"));
         synchronized (lock) {
             try {
                 sqlSession.insert("management.machine.insight.insert", insight);
+                Map<String, Object> condition = new HashMap<>();
+                condition.put("insightId", insight.getInsightId());
+                ResultData response = query(condition);
+                result.setData(((List) response.getData()).get(0));
             } catch (Exception e) {
                 result.setResponseCode(ResponseCode.RESPONSE_ERROR);
                 result.setDescription(e.getMessage());
