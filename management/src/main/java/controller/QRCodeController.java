e51baa8b85d44d51aa52fcbe7dd432d074d5316f
hshydtj9405 <295065492@qq.com>
2017-12-16 13:32:23 +0800

    更新

--- management/src/main/java/controller/QRCodeController.java
+++ management/src/main/java/controller/QRCodeController.java
@@ -460,7 +460,6 @@ public class QRCodeController {
     @RequestMapping(method = RequestMethod.POST, value = "/insight/upload")
     public ResultData upload(@RequestParam MultipartFile file) {
         ResultData result = new ResultData();
-
         String absolutePath = this.getClass().getClassLoader().getResource("").getPath();
         int index = absolutePath.indexOf("/WEB-INF/classes/");
         String basePath = absolutePath.substring(0, index);
