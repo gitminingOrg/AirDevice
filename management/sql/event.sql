f4fd353ad70f2c46380c21f59d97c1dac4548c13
xxh13 <15850781776@163.com>
2018-03-28 18:40:46 +0800

    每天定时求deviceStatus的平均值

--- management/sql/event.sql
+++ management/sql/event.sql
@@ -24,3 +24,23 @@ CREATE EVENT device_status_event
       END |
 
 delimiter ;
+
+delimiter $$
+create event mysql.daily_deviceStatus_event
+on schedule
+every 1 day
+STARTS '2018-03-28 00:00:00'
+do
+BEGIN
+insert into airdevice.air_device_day (
+select deviceStatus.device_id as chip_id,
+avg(deviceStatus.pm25) as pm25,
+date(now()) as date,
+0 as block_flag,
+now() as create_time
+from airdevice.deviceStatus
+where deviceStatus.time > date_sub(now(), interval 24 hour)
+and deviceStatus.power = 1
+group by deviceStatus.device_id
+);
+END $$
